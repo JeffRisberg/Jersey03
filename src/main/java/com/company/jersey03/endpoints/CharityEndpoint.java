f36c2fbd326410f6a13ed053a75ad4e79a0c09b6
JeffRisberg <jrisberg@aol.com>
2018-04-13 19:16:23 -0700

    added example of using readerForUpdating

--- src/main/java/com/company/jersey03/endpoints/CharityEndpoint.java
+++ src/main/java/com/company/jersey03/endpoints/CharityEndpoint.java
@@ -81,6 +81,11 @@ public class CharityEndpoint extends AbstractEndpoint {
             System.out.println(jsonNode.get("website") != null);
 
             Charity tempCharity = new Charity(123, "Dog Foundation", "99-12345", "http://www.dogs.com");
+
+            Charity updatedCharity = objectMapper.readerForUpdating(tempCharity).readValue(requestBody);
+
+            System.out.println(updatedCharity);
+
             return Response.status(Response.Status.OK).entity(tempCharity).build();
         } catch (IOException e) {
             e.printStackTrace();
